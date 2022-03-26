759e673ff28b1dae1144b80f69f4cd9164411fd1
DineshSDR <dineshraja1109@gmail.com>
2022-03-26 22:06:49 +0530

    My project D

---
+++ src/com/match/MatchFixtures.java
@@ -0,0 +1,17 @@
+package com.match;
+
+public class MatchFixtures {
+
+	private void Chennai() {
+		System.out.println("CSK");
+	}
+	
+	public static void main(String[] args) {
+		MatchFixtures fix = new MatchFixtures();
+		fix.Chennai();
+		
+		
+	}
+	
+	
+}
